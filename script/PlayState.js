8e6b0187b222ea6d4f02653cc935673a27d11b4b
bernard <janss.bernard@gmail.com>
2017-05-17 16:20:42 +0200

    ajout base canon projet

--- script/PlayState.js
+++ script/PlayState.js
@@ -4,6 +4,9 @@ let map;
 let layers = {};
 let Groups = {};
 let cursors;
+let spacebar;
+let canon1;
+let canon2;
 
 PlayState.preload = function () {
     game.load.tilemap("map", GLOBAL.DIR.IMAGE + "map.json", null, Phaser.Tilemap.TILED_JSON);
@@ -11,11 +14,14 @@ PlayState.preload = function () {
     game.load.image("background", GLOBAL.DIR.IMAGE + "bg.png");
     game.load.image("base", GLOBAL.DIR.IMAGE + "base.png");
     game.load.image("canon", GLOBAL.DIR.IMAGE + "canon.png");
+    game.load.image("bullet", GLOBAL.DIR.IMAGE + "bullet.png");
 }
 
 PlayState.create = function () {
 
     cursors = this.input.keyboard.createCursorKeys();
+
+    spacebar = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     game.add.sprite(-64, -64, 'background');
 
@@ -38,7 +44,11 @@ PlayState.create = function () {
     let base3 = new Base(Groups.bases['base3'].centerX,Groups.bases['base3'].centerY);
     let base4 = new Base(Groups.bases['base4'].centerX,Groups.bases['base4'].centerY);
 
-    let canon1 = new Canon(Groups.bases['base2'].centerX,Groups.bases['base2'].centerY);
+    canon1 = new Canon(Groups.bases['base1'].centerX,Groups.bases['base1'].centerY);
+    canon2 = new Canon(Groups.bases['base2'].centerX,Groups.bases['base2'].centerY);
+    canon3 = new Canon(Groups.bases['base3'].centerX,Groups.bases['base3'].centerY);
+    canon4 = new Canon(Groups.bases['base4'].centerX,Groups.bases['base4'].centerY);
+
 
 }
 
@@ -49,8 +59,21 @@ PlayState.update = function () {
     //
     // });
 
-    if (cursors.up.isDown) {
-
+    if (cursors.left.isDown) {
+        canon1.turn("left");
+        canon2.turn("left");
+        canon3.turn("left");
+        canon4.turn("left");
+    }
+    if (cursors.right.isDown) {
+        canon1.turn("right");
+        canon2.turn("right");
+        canon3.turn("right");
+        canon4.turn("right");
+    }
+    
+    if(spacebar.justDown){
+        canon1.shoot();
     }
 }
 
